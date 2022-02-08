c58a239e7c895336962d31f4371f832376154b5a
dmitriy_koziuberdin <mitakoz581@gmail.com>
2022-02-08 15:38:21 +0200

    Now (Tue Feb 08 15:31:59 EET 2022) I repeated to use of Java libraries and attached the Date method to display information in main!

---
+++ src/main/java/OOP/com/HelloData.java
@@ -0,0 +1,11 @@
+package OOP.com;
+import java.util.Date;
+/* Для создания класса, котороый будет указывать точную дату и время нужно библиотеку java.util и метод Date*/
+
+public class HelloData {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Dima, now is ");
+        System.out.println(new Date());
+    }
+}
